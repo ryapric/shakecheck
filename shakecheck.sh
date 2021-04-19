1b9f1aaf74f1f69424abbe490c39ba936668f494
Ryan J. Price <ryapric@gmail.com>
2021-04-18 20:35:16 -0500

    Make a little more robust

--- shakecheck.sh
+++ shakecheck.sh
@@ -3,6 +3,10 @@ set -eu
 
 # Lintable Makefile is first script arg
 makefile="$1"
+if [ ! -f "${makefile}" ]; then
+  printf "ERROR: %s is not a valid file\n" "${makefile}" > /dev/stderr
+  exit 1
+fi
 printf ">Checking Makefile at %s...\n" "$(realpath "${makefile}")"
 
 # Grab the defined shell -- users are expected to have set a SHELL, else
@@ -11,8 +15,8 @@ shell=$(
   grep -E '^SHELL' "${makefile}" \
   | grep -o '/.*' \
 ) || {
-  printf "ERROR: Makefiles must define a SHELL Make variable for shakecheck to work!\n" > /dev/stderr
-  exit 1
+  printf "WARNING: 'SHELL' not found; shakecheck will use POSIX 'sh' as the Makefile shell. You can override by setting the 'SHELL' Make variable\n" > /dev/stderr
+  shell='/usr/bin/env sh'
 }
 
 # Build shebang for the implied shell script
@@ -28,5 +32,8 @@ while read -r target; do
   printf "=>Checking target '%s'..." "${target}"
   printf "%s\n" "${shebang}" > /tmp/shakecheck
   make -C "$(dirname "${makefile}")" -n "${target}" >> /tmp/shakecheck
+  if grep 'Nothing to be done for ' /tmp/shakecheck; then
+    continue
+  fi
   shellcheck --exclude=SC2096 /tmp/shakecheck && printf "OK\n"
 done < /tmp/targets
